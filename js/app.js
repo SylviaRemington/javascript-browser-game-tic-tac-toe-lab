4eacbdad25b7856f60493d4c2a83069d95c76505
SylviaRemington <sylviaremington@gmail.com>
2025-05-04 20:28:02 -0700

    addtl note lines 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+//Re-reviewed Intro to JS, Arrays, Objects, Intro to DOM, and DOM Events as of 5/4/2025 
+//and will be completing this lesson with my newly re-reviewed understanding of this material.
+
+
 /*-------------------------------- Constants --------------------------------*/
 
 
